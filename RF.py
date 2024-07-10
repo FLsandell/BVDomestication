23879a619ec6751e0dbb4c394d7c52ccd4d83629
fsandell <137166387+FLsandell@users.noreply.github.com>
2024-07-10 10:47:36 +0200

    Update RF.py

--- RF.py
+++ RF.py
@@ -1,5 +1,5 @@
-# Random Forest script for the publication: Variation analysis employing machine learning reveals domestication patterns and breeding trends in sugar beet
-# this code is only a demonstration of the concept and not ready to use. This has to be adapted by adding input files of choice, output paths. Parameter ranges for hyperparameter optimization have to be selected with proper caution and have to be adapted depending on the input data set. 
+# Trains random forest hyperparameters for predictive models based on sequencing data
+# Code lines to be adapted are marked with e.g. "# insert path to input file".
 # (c) Felix Sandell
 # 1.7.2024
 
@@ -16,9 +16,9 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import train_test_split
 
 # read file
-CHR_input = pd.read_csv('INPUT_FILE',  sep = " ")
+CHR_input = pd.read_csv('# insert path to input file',  sep = " ")
 # Set index
-CHR_input = CHR_input.set_index('ACC')
+CHR_input = CHR_input.set_index('# insert column name of phenotypes')
 # .T
 CHR_input = CHR_input.T
 
@@ -28,14 +28,14 @@ CHR_input = CHR_input.astype('int8')
 
 # creates lists for groups (example BETA_MARITIMA, SUGAR BEET)
 # MARs
-MARs = open("PATH_TO_ACESSION_LIST", "r")
+MARs = open("# insert path to accession list", "r")
 content = MARs.read()
 MARs_list = content.split("\n")
 del MARs_list[-1]
 MARs.close()
 
 # SUGs
-SUGs = open("PATH_TO_ACESSION_LIST", "r")
+SUGs = open("# insert path to accession list", "r")
 content = SUGs.read()
 SUGs_list = content.split("\n")
 del SUGs_list[-1]
@@ -60,15 +60,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y ,random_state = 42, str
 # Parameter Selection
 
 # Number of trees in random forest
-n_estimators = [PARAMETER_RANGE]
+n_estimators = [# insert PARAMETER_RANGE]
 # Number of features to consider at every split
-max_features = [PARAMETER_RANGE]
+max_features = [# insert PARAMETER_RANGE]
 # Maximum number of levels in tree
-max_depth = [PARAMETER_RANGE]
+max_depth = [# insert PARAMETER_RANGE]
 # Minimum number of samples required to split a node
-min_samples_split = [PARAMETER_RANGE]
+min_samples_split = [# insert PARAMETER_RANGE]
 # Minimum number of samples required at each leaf node
-min_samples_leaf = [PARAMETER_RANGE]
+min_samples_leaf = [# insert PARAMETER_RANGE]
 # Method of selecting samples for training each tree
 bootstrap = [True, False]# Create the random grid
 random_grid = {'n_estimators': n_estimators,
